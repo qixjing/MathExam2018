287152bfd02b30e50e2644873782d02bce1e1871
hktttygithub <qqmzhk@163.com>
2018-09-09 23:55:46 +0800

    优化题目难度（二）
    修改类RandomOfSubtract，优化减法的随机数生成规则。
    被减数可以是整数或个位，减数必须是个位，差可以为0。

--- PSP1302/RandomOfSubtract.java
+++ PSP1302/RandomOfSubtract.java
@@ -4,14 +4,15 @@ public class RandomOfSubtract extends RandomNumber {
 	public void createRandomAandB(Bean bean) {
 		// 为方便B的取值，将A的取值范围设置成 [5-9] 和 [10 20 30 ....90]
 		int randomA = 0;
+		int randomB = 0;
 		//当 true时，生成 [5-9], false时，生成 [10 20 30 ....90]
 		if (random.nextBoolean()) {
 			randomA = random.nextInt(5) + 5;
+			randomB = random.nextInt(randomA+1);
 		} else {
 			randomA = 10 * (random.nextInt(9) + 1);
+			randomB = 1+ random.nextInt(9);
 		}
-		// A应该大于等于B
-		int randomB = random.nextInt(randomA + 1);
 		bean.setNumberA(randomA);
 		bean.setNumberB(randomB);
 	}
