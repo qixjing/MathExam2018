823234f97c79502a1c8ee6f3a45fa60e8dea85e3
mujinjia <358925837@qq.com>
2018-09-09 11:30:12 +0800

    实验2的提交，增加了生成小学二年级计算题的功能

--- PSP6313/MathExam6313.java
+++ PSP6313/MathExam6313.java
@@ -9,105 +9,150 @@ public class MathExam6313 {
 	//创建两个StringBuffer对象，用来保存输出的题目和标准答案
 	private static StringBuffer topic = new StringBuffer();
 	private static StringBuffer standAnswer = new StringBuffer();
+	
+	//保存4中运算符号
+	private static String[] Operator = { " + ", " - ", " * ", " / " };
 
 	public static void main(String[] args) {
+		// 判断用户输入的参数是否符合要求
 
-		//判断用户输入的参数是否符合输入要求
-		if(judgmentParameter(args)) {
-			 int len = Integer.parseInt(args[0]);
+		if(judgmentParameter(args)){
+			// 保存用户输入的题目数量
+			int len = Integer.parseInt(args[0]);
+			//保存年级，默认1年级
+			int grade = 1;
+			if(2 == args.length) {
+				 grade = Integer.parseInt(args[1]);
+			}
 			//生成题目
-			generatingTopic(len);
-			//写入文件
+			generatingTopic(len,grade);
+			// 写入文件
 			try {
-				write("out.txt");
-				System.out.println("小学一年级数学题题目已生成，请查看out.txt文件");
+				write313("out.txt");
+				System.out.println("小学" + grade + "年级数学题题目已生成，请查看out.txt文件");
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 	}
 
-	
-	/**
-	 * 
-	 * @param args 用户输入的参数
-	 * @return     当符合要求时返回 true，否则返回false
-	 */
-	private static boolean judgmentParameter(String[] args) {
-		if(0 == args.length) {
-			System.out.println("运行程序时请输入一个数字代表要生成的题数。");
-			return false;
-		}else if(args.length > 1){
-			System.out.println("只需要输入一个数字参数");
-			return false;
-		}else {
-			//去除字符串的前导0
-			String str = args[0].replaceFirst("^0*", "");
-			//检验是否全为数字
-			Pattern pattern = Pattern.compile("[0-9]*");
-		    boolean matches = pattern.matcher(str).matches(); 
-		    if(matches && str.length() > 4) {
-				System.out.println("参数数字过大，请重新运行，输入参数");
-				return false;
-		    }else if(matches) {
-		    	return true;
-		    }else {
-				System.out.println("输入的参数不是正整数，请重新运行，输入一个正整数");
-				return false;
-		    }
-		}
-	}
-	
-	
+
 	/**
 	 * 作用：生成题目
 	 * @param len 用户要求生成的题目数量
+	 * @param grade 年级
 	 */
-	private static void generatingTopic(int len) {
-		for(int i = 1; i <= len; i++) {
-			//获取两个随机数，num1,num2表示参与计算的两个数字; 
-			int num1 = (int) (Math.random()*101);
-			int num2 = (int) (Math.random()*101);
-			//确保第一个数比第二个数大，避免相减出现负数，小学一年级加减无负数
-			if(num1 < num2) {
+	private static void generatingTopic(int len,int grade) {
+		for (int i = 1; i <= len; i++) {
+			// 获取两个随机数，num1,num2表示参与计算的两个数字;
+			int num1 = (int) (Math.random() * 101);
+			int num2 = (int) (Math.random() * 101);
+			// 确保第一个数比第二个数大，避免相减出现负数，小学加减无负数
+			if (num1 < num2) {
 				int temp = num1;
 				num1 = num2;
 				num2 = temp;
 			}
-			//symbol代表运算符号，当随机数为0得到  " + ",随机数为1得到" - ";
-			String symbol = (0 == ((int) (Math.random()*10))%2)? " + " : " - ";
-			//计算结果
-			int res = (symbol.equals(" + ")) ? num1 + num2 :num1 - num2;
-			//将题目和答案记录
+			// symbol代表运算符号;
+			int index = (1 == grade) ? ((int) (Math.random() * 10)) % 2 : ((int) (Math.random() * 10)) % 4;
+			String symbol = Operator[index];
+			// 计算结果
+			int res = 0;
+			int remainder = 0; // 余数
+			switch (symbol) {
+			case " + ":
+				res = num1 + num2;
+				break;
+			case " - ":
+				res = num1 - num2;
+				break;
+			case " * ":
+				res = num1 * num2;
+				break;
+			case " / ":
+				res = num1 / num2;
+				remainder = num1 % num2;
+				break;
+			}
+			// 将题目和答案记录
 			topic.append("(" + i + ") " + num1 + symbol + num2 + System.lineSeparator());
-			standAnswer.append("(" + i + ") " + num1 + symbol + num2 + " = " + res + System.lineSeparator());
+			if (symbol.equals(" / ")) {
+				standAnswer.append("(" + i + ") " + num1 + symbol + num2 + " = " + res
+						+ (remainder != 0 ? ("..." + remainder) : "") + System.lineSeparator());
+			} else {
+				standAnswer.append("(" + i + ") " + num1 + symbol + num2 + " = " + res + System.lineSeparator());
+			}
 		}
 	}
+
 	
+	/**
+	 * 作用：判断用户输入的参数是否合法
+	 * @param args 用户输入的参数
+	 * @return     当符合要求时返回 true，否则返回false
+	 */
+	private static boolean judgmentParameter(String[] args) {
+		if (0 == args.length) {
+			System.out.println("运行程序时请输入两个数字代表要生成的题数和年级。");
+			return false;
+		} else if (args.length > 2) {
+			System.out.println("最多输入两个个数字参数，第一个代表题目数量，第二个代表年级");
+			return false;
+		} else {
+
+			//去除第一个参数的前导0
+			String str1 = args[0].replaceFirst("^0*", "");
+			// 检验第一个参数是否都是数字
+			Pattern pattern = Pattern.compile("[0-9]*");
+			boolean matches = pattern.matcher(str1).matches();
+
+			if (matches && str1.length() > 4) {
+				System.out.println("第一个参数数字过大，请重新运行，输入参数");
+				return false;
+			} else if (!matches) {
+				System.out.println("输入的第一个参数不是正整数，请重新运行，输入一个正整数");
+				return false;
+			}
+
+			if (2 == args.length) {
+				//去除第二个个参数的前导0
+				String str2 = args[1].replaceFirst("^0*", "");
+				// 检验第一个参数是否都是1或2
+				Pattern compile = Pattern.compile("[1-2]?");
+				boolean matches2 = compile.matcher(str2).matches();
+				
+				if (!matches2) {
+					System.out.println("输入的第二个参数不是1或2，请重新运行，第二个参数输入1或2");
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
 	
 	/**
 	 * 作用：将生成的题目和和答案写入指定文件
-	 * @param filename     将要写入的文件名
-	 * @throws IOException 当文件名不合法时抛出异常
+	 * @param filename       将要写入的文件名
+	 * @throws IOException   当文件名不合法时抛出异常
 	 */
-	private static void write(String filename) throws IOException {
-	    // 步骤1：确定输出的文件（目的地）
+	private static void write313(String filename) throws IOException {
+		// 步骤1：确定输出的文件（目的地）
 		// 如果filename中包含路径，必须确保路径已存在
-		  File file = new File(filename);
-		  File parentFile = file.getParentFile();
-		  if(parentFile!=null&&!parentFile.exists()) {
-			  parentFile.mkdirs();
-			  System.out.println("创建目录：" + parentFile);
-		  }
-		  file.createNewFile();
+		File file = new File(filename);
+		File parentFile = file.getParentFile();
+		if (parentFile != null && !parentFile.exists()) {
+			parentFile.mkdirs();
+			System.out.println("创建目录：" + parentFile);
+		}
+		file.createNewFile();
 		// 步骤2：创建指向文件的输出流
-		  OutputStream out = new FileOutputStream(file);
+		OutputStream out = new FileOutputStream(file);
 		// 步骤3：写入数据
-		  out.write(topic.toString().getBytes());
-		  out.write(System.lineSeparator().getBytes());
-		  out.write(standAnswer.toString().getBytes());
-		  
-	    // 步骤4：关闭
-		  out.close();
-	  }
+		out.write(topic.toString().getBytes());
+		out.write(System.lineSeparator().getBytes());
+		out.write(standAnswer.toString().getBytes());
+		// 步骤4：关闭
+		out.close();
+	}
 }
