f29fff0b443ddb31269490013eff1bf99b55c210
HYOUKACC <HYOUKACC@users.noreply.github.com>
2018-09-09 19:52:25 +0800

    完整版代码推送
    符合测试要求的完整代码

--- PSP4331/Operation.java
+++ PSP4331/Operation.java
@@ -1,10 +1,11 @@
 
-
+//运算父类
 
 
 public class Operation {
 	int firstNumber; //第一个随机数
 	int secondNumber; //第二个随机数
+	int remainder;//余数
 	int answerNumber; //答案
 	
 	public int getFirstNumber() {
--- PSP4331/OutPutSet.java
+++ PSP4331/OutPutSet.java
@@ -1,6 +1,6 @@
 
 
-import java.io.File;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
