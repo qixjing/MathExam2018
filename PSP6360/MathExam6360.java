7b0a2c037a940d5c591a3bd9d398c124da5c7b83
Dnyqi <42905518+a704218021@users.noreply.github.com>
2018-09-10 21:41:01 +0800

    MathExam6360（3）
    create方法 增加一位参数，表示年级，可以生成二年级的加减乘除计算题。
    第三个随机数的区间从0-99改为1-99。
    在main方法里获取转化为整型的参数，并把获取的参数传给create 。

--- PSP6360/MathExam6360.java
+++ PSP6360/MathExam6360.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.util.Random;
 public class MathExam6360 {
 	
-	public static void create(int count) {
+	public static void create(int count,int grade) {
 		File file = new File ("out.txt");//创建文本
 		FileWriter fw=null;
 			try {
@@ -19,10 +19,11 @@ public class MathExam6360 {
 			}	
 			BufferedWriter bw = new BufferedWriter(fw);
 		int first=0,symbol=0,second=0,sign=1,mark=0,w=0;
-		int [] an=new int[count+5];
-		int [] bn=new int[count+5];
-		int [] cn=new int[count+5];
-		int [] dn=new int[count+5];
+		int [] an=new int[count];
+		int [] bn=new int[count];
+		int [] cn=new int[count];
+		int [] dn=new int[count];
+		int [] en=new int[count];
 		Random r =new Random(); 
 		while(sign<=count) {
 			if(mark==0) {//判断题号是否需要更新，若需要则输入题号。
@@ -40,46 +41,76 @@ public class MathExam6360 {
 			first=number;}
 			else if(i==1)
 			{
-			 int number = (int)Math.floor((r.nextDouble()*2.0));//生成第二个数字，用于判断符号
-			 symbol=number;
+				if(grade==1) 
+				{
+					int number = (int)Math.floor((r.nextDouble()*2.0));//生成第二个数字，用于判断符号
+					symbol=number;
+				}
+				else if(grade==2)
+				{
+					int number = (int)Math.floor((r.nextDouble()*4.0));//生成第二个数字，用于判断符号
+					symbol=number;
+				}
+				
 			}
 			else if(i==2)
-			{int number = (int)Math.floor((r.nextDouble()*100.0));//生成第三个数字
-				second=number;}		
+			{	
+				int number = 1+(int)Math.floor((r.nextDouble()*100.0));//生成第三个数字//生成第三个数字
+				second=number;
+			}		
 			else if(i==3)
 			{
-			 if (symbol==0)//判断符号，0为+号，1为-号
-			 {  try {
-				bw.write(String.valueOf(first+" + "+second+" = "));
-				
-			} catch (IOException e) {
-				// TODO 自动生成的 catch 块
-				e.printStackTrace();
-			}
+				en[w]=0;
+				if (symbol==0)//判断符号，0为+号，1为-号，2为×号，3为÷号
+				{   
+					try {
+						bw.write(String.valueOf(" "+ first+ " + " +second+" = "));
+					} catch (IOException e) {
+						// TODO 自动生成的 catch 块
+						e.printStackTrace();
+					}
+					an[w]=first+second;
 				}
-			 else if(symbol==1)//判断符号，0为+号，1为-号
-			 {
-				 if(first-second < 0)//判断式子的结果是否为负数，若是，则退出内循环，重新开始while循环。
+				else if((symbol==1))
+				{
+					if(first-second < 0)
 					{mark=1;break;}
-				try {
-					bw.write(String.valueOf(first+" - "+second+" = "));
-				} catch (IOException e) {
-					// TODO 自动生成的 catch 块
-					e.printStackTrace();
+					try {
+						bw.write(String.valueOf(" "+ first+ " - " +second+" = "));
+					} catch (IOException e) {
+						// TODO 自动生成的 catch 块
+						e.printStackTrace();
+					}
+					an[w]=first-second;
 				}
-			 	
-			 	}
-				if(symbol==0)//判断符号，然后把答案存入数组an
-					{an[w]=first+second;}
-				else if(symbol==1)
-					{an[w]=first-second;}
-
+				else if((symbol==2)) 
+				{
+					try {
+						bw.write(String.valueOf(" "+ first+ " × " +second+" = "));
+					} catch (IOException e) {
+						// TODO 自动生成的 catch 块
+						e.printStackTrace();
+					}
+					an[w]=first*second;
+				}
+				else if((symbol==3)) 
+				{
+					en[w]=first % second;
+					an[w]=(int)(first/second);
+					try {
+						bw.write(String.valueOf(" "+ first+ " ÷ " +second+" = "));
+					} catch (IOException e) {
+						// TODO 自动生成的 catch 块
+						e.printStackTrace();
+					}
+				}
 				bn[w]=first; //将式子的数字存入数组中，方便之后的提取
 				cn[w]=symbol;
 				dn[w]=second;
 				sign++;
-			 	mark=0;//用于判断式子是否需要更新题号、
-				w++;
+				mark=0;//证明式子成功写入，题号需要更新。
+				w++;	
+				
 			}
 			
 		}
@@ -104,23 +135,58 @@ public class MathExam6360 {
 		{
 			if(cn[i]==0) {
 				try {
-					bw.write(String.valueOf("(" + (i+1) +") "+ bn[i]+" + "+dn[i]+" = "+an[i]));
-					bw.newLine();
+					bw.write(String.valueOf("("+(i+1)+") "+ bn[i]+ " + " +dn[i]+" = "+an[i]));
 				} catch (IOException e) {
 					// TODO 自动生成的 catch 块
 					e.printStackTrace();
 				}
-				
 			}
 			else if(cn[i]==1) {
 				try {
-					bw.write(String.valueOf("(" + (i+1) +") "+ bn[i]+" - "+dn[i]+" = "+an[i]));
-					bw.newLine();
+					bw.write(String.valueOf("("+(i+1)+") "+ bn[i]+ " - " +dn[i]+" = "+an[i]));
 				} catch (IOException e) {
 					// TODO 自动生成的 catch 块
 					e.printStackTrace();
+				}
+				}
+			else if(cn[i]==2) {
+				try {
+					bw.write(String.valueOf("("+(i+1)+") "+ bn[i]+ " × " +dn[i]+" = "+an[i]));
+				} catch (IOException e) {
+					// TODO 自动生成的 catch 块
+					e.printStackTrace();
+				}
+
+				}
+			else if(cn[i]==3) 
+			{
+				if(en[i]==0) 
+				{
+					try {
+						bw.write(String.valueOf("("+(i+1)+") "+ bn[i]+ " ÷ " +dn[i]+" = "+an[i]));
+					} catch (IOException e) {
+						// TODO 自动生成的 catch 块
+						e.printStackTrace();
 					}
+				}
+				else if(en[i]!=0)
+				{
+					try {
+						bw.write(String.valueOf("("+(i+1)+") "+ bn[i]+ " ÷ " +dn[i]+" = "+an[i]+ " ··· "+ en[i]));
+					} catch (IOException e) {
+						// TODO 自动生成的 catch 块
+						e.printStackTrace();
+					}
+
+				}
 			}
+	
+		try {
+			bw.newLine();
+		} catch (IOException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		}
 		}
 		try {
 				bw.close();
@@ -134,8 +200,8 @@ public class MathExam6360 {
 		// TODO 自动生成的方法存根
 		int num[] = new int [args.length];//创建数组，用于读取参数
 		num [0] = Integer.parseInt(args[0]);//将参数转化为整型
-		create(num[0]);
-		
+		num [1] = Integer.parseInt(args[1]);//将参数转化为整型
+		create(num[0],num[1]);
 		}	
 	}
 
