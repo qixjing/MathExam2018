316f2e43c1c09e4236399e3361a37f01d22dd7ba
LinKai945 <33064919+LinKai945@users.noreply.github.com>
2018-09-10 20:05:30 +0800

    MathExam4269.java：优化出题逻辑
    修改了文件MathExam4269.java:优化了出题逻辑,使其更符合小学一年级的作业水平,不会再出现相加超过100或者相减小于0的情况

--- PSP4269/MathExam4269.java
+++ PSP4269/MathExam4269.java
@@ -6,46 +6,74 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 
 public class MathExam4269 {
+	static int a;
+	static int b;
+	static int sym;
+
+	public static boolean check() {
+		a = (int) (0 + Math.random() * (100 - 0 + 1));
+		b = (int) (0 + Math.random() * (100 - 0 + 1));
+		sym = (int) (1 + Math.random() * (2 - 1 + 1));
+		if (sym == 1) {
+			if ((a + b) > 100) {
+				return false;
+			} else {
+				return true;
+			}
+		} else {
+			if ((a - b) < 0) {
+				return false;
+			} else {
+				return true;
+			}
+
+		}
+
+	}
+
 	public static void math(int n) {
 		String prob;
 		String ans;
-		ArrayList<String> probs=new ArrayList<String>();
-		ArrayList<String> anss=new ArrayList<String>();
-		for(int i=1;i<=n;i++) {
-			int a=(int)(1+Math.random()*(99-1+1));
-			int b=(int)(1+Math.random()*(99-1+1));
-			int sym=(int)(1+Math.random()*(2-1+1));
-			if(sym==1) {
-				prob="("+i+")"+" "+a+" "+"+"+" "+b;
-				 
-				ans="("+i+")"+" "+a+" "+"+"+" "+b+" "+"="+" "+(a+b);
-			}else{
-				prob="("+i+")"+" "+a+" "+"-"+" "+b;
-				ans="("+i+")"+" "+a+" "+"-"+" "+b+" "+"="+" "+(a-b);
+		ArrayList<String> probs = new ArrayList<String>();
+		ArrayList<String> anss = new ArrayList<String>();
+		boolean end = false;
+		for (int i = 1; i <= n; i++) {
+			do {
+				end = check();
+			} while (!end);
+
+			if (sym == 1) {
+				prob = "(" + i + ")" + " " + a + " " + "+" + " " + b;
+				ans = "(" + i + ")" + " " + a + " " + "+" + " " + b + " " + "=" + " " + (a + b);
+			} else {
+				prob = "(" + i + ")" + " " + a + " " + "-" + " " + b;
+				ans = "(" + i + ")" + " " + a + " " + "-" + " " + b + " " + "=" + " " + (a - b);
 			}
 			probs.add(prob);
 			anss.add(ans);
+
 		}
+
 		File file = new File("out.txt");
 		if (!file.exists()) {
 			File parent = file.getParentFile();
 			if (parent != null && !parent.exists()) {
 				parent.mkdirs();
 			}
-				try {
-					file.createNewFile();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					System.out.println("文件创建失败,请重试");
-					e.printStackTrace();					
-				}
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				System.out.println("文件创建失败,请重试");
+				e.printStackTrace();
+			}
 		}
 		try {
 			OutputStream out = new FileOutputStream(file);
 			byte[] data;
 			for (String str : probs) {
-				str+="\r\n";
-				data=str.getBytes();
+				str += "\r\n";
+				data = str.getBytes();
 				try {
 					out.write(data);
 				} catch (IOException e) {
@@ -54,8 +82,8 @@ public class MathExam4269 {
 					e.printStackTrace();
 				}
 			}
-			String fg="-----------------标准答案-----------------"+"\r\n";
-			data=fg.getBytes();
+			String fg = "-----------------标准答案-----------------" + "\r\n";
+			data = fg.getBytes();
 			try {
 				out.write(data);
 			} catch (IOException e) {
@@ -64,8 +92,8 @@ public class MathExam4269 {
 				e.printStackTrace();
 			}
 			for (String str : anss) {
-				str+="\r\n";
-				data=str.getBytes();
+				str += "\r\n";
+				data = str.getBytes();
 				try {
 					out.write(data);
 				} catch (IOException e) {
@@ -86,10 +114,11 @@ public class MathExam4269 {
 			System.out.println("找不到文件,请重试");
 			e.printStackTrace();
 		}
-	
+
 	}
+
 	public static void main(String[] args) {
-		int n=Integer.parseInt(args[0]);
+		int n = Integer.parseInt(args[0]);
 		math(n);
 	}
 }
