6a560fbf8857f6f5168c06619c68779f5c11fe1e
linzenghan <42675313+linzenghan@users.noreply.github.com>
2018-09-10 20:13:16 +0800

    实现小学生一二年级四则运算
    修改了一些注释并且改用参数输入

--- PSP6318/MathExam6318.java
+++ PSP6318/MathExam6318.java
@@ -1,7 +1,5 @@
 
 
-
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.util.Scanner;
@@ -16,33 +14,37 @@ public class MathExam6318 {
            System.out.println("文件创建完成");
         }
 		
-		int n,i=1,grade;
+		
+		
+		int []n =new int [args.length] ;
+	    n[0] = Integer.parseInt(args[0]);
+	    n[1] = Integer.parseInt(args[1]);
+
+		int i=1;
 		Scanner sc=new Scanner(System.in);
 		System.out.println("请依次输入要生成的题数与年级");
-		n=sc.nextInt();
-		grade=sc.nextInt();
-		String [] as=new String[n+1];
-		String [] bs=new String[n+1];	
-		String [] cs=new String[n+1];	
+		String [] as=new String[n[0]+1];
+		String [] bs=new String[n[0]+1];	
+		String [] cs=new String[n[0]+1];	
 		StringBuffer qs = null;
 		StringBuffer rs = null;
 		FileOutputStream in=new FileOutputStream(file);
-		if(grade==1) {
-		for(i=0;i<n;i++)
-		{
+		if(n[1]==1) {
+		for(i=0;i<n[0];i++)
+		{													
 		int a=(int)(Math.random()*100+1);
 		int b=(int)(Math.random()*a+1);
 		int c=(int)(Math.random()*2);
 		if(c%2==0) {
-			System.out.println("("+(i+1)+")"+a+"+"+b+"=");
-			qs = new StringBuffer("("+(i+1)+")"+a+"+"+b+"=");
+			System.out.println("("+(i+1)+")"+a+"+"+b+"=");			//加法运算
+			qs = new StringBuffer("("+(i+1)+")"+a+"+"+b+"=");	
 			rs = new StringBuffer(" "+(a+b));
 			as[i]=("("+(i+1)+")"+a+"+"+b+"=");
 			qs.append(rs);
 			cs[i]=qs.toString();
 			}
 			else {
-			System.out.println("("+(i+1)+")"+a+"-"+b+"=");
+			System.out.println("("+(i+1)+")"+a+"-"+b+"=");			//减法运算
 			qs = new StringBuffer("("+(i+1)+")"+a+"-"+b+"=");
 			rs = new StringBuffer(" "+(a-b));
 			as[i]=("("+(i+1)+")"+a+"-"+b+"=");
@@ -57,14 +59,14 @@ public class MathExam6318 {
 		}
 		
 		String daan ="-------------标准答案--------------";
-		byte[] pa = daan.getBytes();	
+		byte[] pa = daan.getBytes();									//写入“标准答案”
 		String ka ="\n\r";
 		byte[] ra = ka.getBytes(); 
 		in.write(pa);	
 		in.write(ra);
 		System.out.println("-------------标准答案--------------");
-		for(i=0;i<n;i++)
-		{
+		for(i=0;i<n[0];i++)
+		{																//输出答案并写入文本
 			System.out.println(cs[i]);
 			byte[] ta = cs[i].getBytes();
 			in.write(ta);
@@ -74,13 +76,13 @@ public class MathExam6318 {
 				
 				}
 		else {
-			for(i=0;i<n;i++)
+			for(i=0;i<n[0];i++)								
 			{
-			int a=(int)(Math.random()*10+1);
+			int a=(int)(Math.random()*10+1);						
 			int b=(int)(Math.random()*10+1);
 			int c=(int)(Math.random()*2);
 			if(c%2==0) {
-				System.out.println("("+(i+1)+")"+a+"x"+b+"=");
+				System.out.println("("+(i+1)+")"+a+"x"+b+"=");				//乘法运算
 				qs = new StringBuffer("("+(i+1)+")"+a+"x"+b+"=");
 				rs = new StringBuffer(" "+(a*b));
 				as[i]=("("+(i+1)+")"+a+"x"+b+"=");
@@ -89,8 +91,8 @@ public class MathExam6318 {
 				}
 			else {
 				if(a%b==0) {
-				System.out.println("("+(i+1)+")"+a+"÷"+b+"=");
-				qs = new StringBuffer("("+(i+1)+")"+a+"÷"+b+"=");
+				System.out.println("("+(i+1)+")"+a+"÷"+b+"=");				//除法运算（无余数）
+				qs = new StringBuffer("("+(i+1)+")"+a+"÷"+b+"=");			
 				rs = new StringBuffer(" "+(a/b));
 				as[i]=("("+(i+1)+")"+a+"÷"+b+"=");
 				qs.append(rs);
@@ -98,7 +100,7 @@ public class MathExam6318 {
 				
 				}
 				else {
-				System.out.println("("+(i+1)+")"+a+"÷"+b+"=");
+				System.out.println("("+(i+1)+")"+a+"÷"+b+"=");			//除法运算（有余数）
 				qs = new StringBuffer("("+(i+1)+")"+a+"÷"+b+"=");
 				rs = new StringBuffer(" "+(a/b)+"..."+a%b);
 				as[i]=("("+(i+1)+")"+a+"÷"+b+"=");
@@ -109,23 +111,22 @@ public class MathExam6318 {
 				
 				}
 				
-				byte[] ba = as[i].getBytes();
+				byte[] ba = as[i].getBytes();							//写入题目（乘与除）
 				String ka ="\n\r";
 				byte[] ra = ka.getBytes(); 
-				
 				in.write(ba);
 				in.write(ra);	
 			}
 			String daan ="-------------标准答案--------------";
-			byte[] pa = daan.getBytes();	
+			byte[] pa = daan.getBytes();							//写入“标准答案”
 			String ka ="\n\r";
 			byte[] ra = ka.getBytes(); 
 			in.write(pa);
 			in.write(ra);
 		
 		System.out.println("-------------标准答案--------------");
-		for(i=0;i<n;i++)
-		{
+		for(i=0;i<n[0];i++)
+		{															//输出答案并写入题目+答案
 			System.out.println(cs[i]);
 			byte[] ta = cs[i].getBytes();
 			in.write(ta);
