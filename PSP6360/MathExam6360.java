ee50415e01cf37e562c27eb1c431dbe628814a6c
Dnyqi <42905518+a704218021@users.noreply.github.com>
2018-09-10 19:19:00 +0800

    MathExam6360（2）
    去掉抛异常throws Exception，更改为使用try catch 处理异常。
    删除 int 变量  q 和  flag，创建方法 create，在主函数调用create方法。
    将bw.write整合在一起，统一输出。

--- PSP6360/MathExam6360.java
+++ PSP6360/MathExam6360.java
@@ -3,32 +3,35 @@
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Random;
 public class MathExam6360 {
-
-	public static void main(String[] args) throws Exception {
-		// TODO 自动生成的方法存根
-		File file = new File ("out.txt");//创建文本
-		int num[] = new int [args.length];//创建数组，用于读取参数
 	
-		if(!file.exists())//判断文件是否存在，不存在侧创建
-		{
-		file.createNewFile();
-		System.out.println("out.txt创建完成");
-		}
-		FileWriter fw = new FileWriter(file);
-		BufferedWriter bw = new BufferedWriter(fw);
-		num [0] = Integer.parseInt(args[0]);//将参数转化为整型
-		int first=0,symbol=0,second=0,sign=1,q=0,mark=0,w=0;
-		int [] an=new int[num[0]];
-		int [] bn=new int[num[0]];
-		int [] cn=new int[num[0]];
-		int [] dn=new int[num[0]];
-		
+	public static void create(int count) {
+		File file = new File ("out.txt");//创建文本
+		FileWriter fw=null;
+			try {
+				fw = new FileWriter(file);
+			
+			} catch (IOException e) {
+				// TODO 自动生成的 catch 块
+				e.printStackTrace();
+			}	
+			BufferedWriter bw = new BufferedWriter(fw);
+		int first=0,symbol=0,second=0,sign=1,mark=0,w=0;
+		int [] an=new int[count+5];
+		int [] bn=new int[count+5];
+		int [] cn=new int[count+5];
+		int [] dn=new int[count+5];
 		Random r =new Random(); 
-		while(sign<=num[0]) {
+		while(sign<=count) {
 			if(mark==0) {//判断题号是否需要更新，若需要则输入题号。
-		bw.write(String.valueOf("("+sign+")"+" "));
+		try {
+			bw.write(String.valueOf("("+sign+") "));
+		} catch (IOException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		}
 			}
 		for(int i=0;i<4;i++)//循环四次，随机生成三个数。并在第四次进行符号判断，最后在文本输入式子。
 		{
@@ -46,92 +49,96 @@ public class MathExam6360 {
 			else if(i==3)
 			{
 			 if (symbol==0)//判断符号，0为+号，1为-号
-			 {  bw.write(String.valueOf(" "+ first+" "));
-				bw.write(String.valueOf("+"));
-			 	bw.write(String.valueOf(" "+second+" "));
-			 	bw.write(String.valueOf("="));
-				sign++;
-				mark=0;//证明式子成功写入，题号需要更新。
-				bn[w]=first;
-				cn[w]=0;
-				dn[w]=second;
-				w++;
+			 {  try {
+				bw.write(String.valueOf(first+" + "+second+" = "));
+				
+			} catch (IOException e) {
+				// TODO 自动生成的 catch 块
+				e.printStackTrace();
+			}
 				}
 			 else if(symbol==1)//判断符号，0为+号，1为-号
 			 {
 				 if(first-second < 0)//判断式子的结果是否为负数，若是，则退出内循环，重新开始while循环。
 					{mark=1;break;}
-				bw.write(String.valueOf(" "+ first+" "));
-				bw.write(String.valueOf("-"));
-				bw.write(String.valueOf(" "+second+" "));
-				bw.write(String.valueOf("="));
-			 	sign++;
-			 	mark=0;
-				bn[w]=first;
-				cn[w]=1;
-				dn[w]=second;
-				w++;
+				try {
+					bw.write(String.valueOf(first+" - "+second+" = "));
+				} catch (IOException e) {
+					// TODO 自动生成的 catch 块
+					e.printStackTrace();
+				}
+			 	
 			 	}
-			for(;;)
-			 {
 				if(symbol==0)//判断符号，然后把答案存入数组an
-				{	an[q]=first+second;q++;break;}	
+					{an[w]=first+second;}
 				else if(symbol==1)
-				{	an[q]=first-second;q++;break;}
-			 }
+					{an[w]=first-second;}
+
+				bn[w]=first; //将式子的数字存入数组中，方便之后的提取
+				cn[w]=symbol;
+				dn[w]=second;
+				sign++;
+			 	mark=0;//用于判断式子是否需要更新题号、
+				w++;
 			}
 			
 		}
 		if(mark==0)
-		bw.newLine();
-		}
-		bw.write(String.valueOf("-----------------标准答案 -----------------------"));
-		bw.newLine();
-		int flag=1;
-		for(int i=0;i<num[0];i++)//循环输出答案，利用数组。
-		{
-			if(cn[i]==0) {
-			bw.write(String.valueOf("(" + flag +")"));
-			bw.write(String.valueOf(" "+ bn[i]+" "));
-			bw.write(String.valueOf("+"));
-			bw.write(String.valueOf(" "+dn[i]+" "));
-			bw.write(String.valueOf("="));
-			bw.write(String.valueOf(" "+an[i]));
-			bw.newLine();
-			flag++;
-			}
-			else if(cn[i]==1) {
-			bw.write(String.valueOf("(" + flag +")"));
-			bw.write(String.valueOf(" "+ bn[i]+" "));
-			bw.write(String.valueOf("-"));
-			bw.write(String.valueOf(" "+dn[i]+" "));
-			bw.write(String.valueOf("="));
-			bw.write(String.valueOf(" "+an[i]));
-			bw.newLine();
-			flag++;
+			try {
+				bw.newLine();
+			} catch (IOException e) {
+				// TODO 自动生成的 catch 块
+				e.printStackTrace();
 			}
 		}
-		bw.close();
-		fw.close();
-		}
-}
-	/*	while((data=BR.readLine())!=null) {
-		bw.write(String.valueOf(data));		
-		bw.write(String.valueOf(' '));
-		bw.write(String.valueOf(an[q]));
-		bw.newLine();
-		bw.flush();
-		q++;		
+		try {
+			bw.write(String.valueOf("-----------------标准答案 -----------------------"));
+			bw.newLine();
+		} catch (IOException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
 		}
 		
-		BR.close();
-		fr.close();
-		//cd eclipse/作业/作业1/01work6360/src
-	 for(q=0;q<num[0];q++)
+
+		for(int i=0;i<count;i++)//循环输出答案，利用数组。
 		{
-		bw.write(String.valueOf(an[q]));
-		bw.newLine();}
-		*/
+			if(cn[i]==0) {
+				try {
+					bw.write(String.valueOf("(" + (i+1) +") "+ bn[i]+" + "+dn[i]+" = "+an[i]));
+					bw.newLine();
+				} catch (IOException e) {
+					// TODO 自动生成的 catch 块
+					e.printStackTrace();
+				}
+				
+			}
+			else if(cn[i]==1) {
+				try {
+					bw.write(String.valueOf("(" + (i+1) +") "+ bn[i]+" - "+dn[i]+" = "+an[i]));
+					bw.newLine();
+				} catch (IOException e) {
+					// TODO 自动生成的 catch 块
+					e.printStackTrace();
+					}
+			}
+		}
+		try {
+				bw.close();
+				fw.close();
+			} catch (IOException e) {
+				// TODO 自动生成的 catch 块
+				e.printStackTrace();
+				}
+	}
+	public static void main(String[] args)  {
+		// TODO 自动生成的方法存根
+		int num[] = new int [args.length];//创建数组，用于读取参数
+		num [0] = Integer.parseInt(args[0]);//将参数转化为整型
+		create(num[0]);
+		
+		}	
+	}
+
 		
 		
 
