671a39807149aac3df1c7c09c4263950a81acaff
YHQ0215 <42902085+YHQ0215@users.noreply.github.com>
2018-09-11 13:23:30 +0800

    小学出题器
    修改了 命令行参数接收一个n 以及添加时间和学号姓名

--- PSP6387/MathExam6387.java
+++ PSP6387/MathExam6387.java
@@ -2,21 +2,23 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
-import java.util.Scanner;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+// import java.util.Scanner;
 
 
 public class MathExam6387 {
 
-	private static Scanner in;
+// 	private static Scanner in;
 	private static int x,y,f;
 	private static String[] str;
 
 	public static void main(String[] args) throws FileNotFoundException, InterruptedException {
 		// TODO Auto-generated method stub
-		int n ;
-		in = new Scanner(System.in);
-		n = in.nextInt();
-		
+// 		int n ;
+// 		in = new Scanner(System.in);
+// 		n = in.nextInt();
+		int i =Integer.valueOf(args[0]).intValue();
 		File file = new File("out6387.txt");
 		PrintStream ps = new PrintStream(file);
 		System.setOut(ps);
@@ -39,8 +41,10 @@ public class MathExam6387 {
 				str[i] = "(" + (i+1) + ")" +" " + x + "-" + y + " " + "=" + " " + (x-y);
 			}
 		}
-		System.out.println("--------��׼��--------");
+		System.out.println("--------标准答案--------");
 		    for(String a:str)
 		        System.out.println(a);
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日 HH:mm");
+		    System.out.println("              211606387 姓名：叶宏奇  " +sdf.format(new Date()));
 	}
 }
