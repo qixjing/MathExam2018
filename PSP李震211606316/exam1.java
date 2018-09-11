be97fa12f42f1eef6f5aaa48d52fae54539f8334
1429189566 <1429189566@qq.com>
2018-09-11 18:13:57 +0800

    new
    增加生成文本

--- PSP李震211606316/exam1.java
+++ PSP李震211606316/exam1.java
@@ -5,10 +5,8 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.util.Scanner;
 
-
 public class exam1 {
 	public static void main(String[] args) throws FileNotFoundException {
-		//确定输出的文件
 		File file = new File("out.txt");
 		//创建指向文件的数据输出流
 		DataOutputStream out = new DataOutputStream(new FileOutputStream(file));
@@ -22,6 +20,7 @@ public class exam1 {
 			grade_two();
 		}
 		}
+
 	
 		public static void grade_one() {	
 			System.out.println("请输入需要出题的数目：");
@@ -30,6 +29,7 @@ public class exam1 {
 			int ai[]=new int[n];
 			int aj[]=new int[n];
 			int ax[]=new int[n];
+			int m1=1;
 			for(int a=0; a<n;a++)
 			{
 				int i=(int) Math.floor(Math.random()*20+1);
@@ -38,18 +38,16 @@ public class exam1 {
 				ai[a]=i;
 				aj[a]=j;
 				ax[a]=x;
-				int m1=1;
+//				int m1=1;
 				ax[a]=x;
 				if(x%2==1) {
 				System.out.println("(" + m1 + ") "+ i +" + " + j + "=");
 				}else {
-					if(i>j){
 				System.out.println("(" + m1 + ") "+ i + " - "+j+"= ");
-				}else {
-					System.out.println("(" + m1 + ") "+ j + " - "+i+"= ");
-					}
+				}
 				m1++;
-			}	
+				}
+				
 			System.out.println("                  ");		
 			int m2=1;
 			for(int a1=0;a1<n;a1++)
@@ -57,18 +55,13 @@ public class exam1 {
 			if(ax[a1]%2==1) 
 			{
 			System.out.println("("+ m2 +") "+ ai[a1] +" + "+ aj[a1] +" = "+(ai[a1]+aj[a1]));
-			String str = ("("+ m2 +") "+ ai[a1] +" + "+ aj[a1] +" = "+(ai[a1]+aj[a1]));
 			}else {
 			System.out.println("("+ m2 +") "+ ai[a1] +" - "+ aj[a1] +" = "+(ai[a1]-aj[a1]));
-			String str = ("("+ m2 +") "+ ai[a1] +" - "+ aj[a1] +" = "+(ai[a1]-aj[a1]));
 			}
 			m2++;
-			
 		}
-	}	
-}
-		
-		
+	}
+			
 		public static void grade_two() {
 			System.out.println("请输入需要出题的数目：");
 			Scanner s=new Scanner(System.in);
@@ -76,6 +69,7 @@ public class exam1 {
 			double ai[]=new double[m];
 			double aj[]=new double[m];
 			double ax[]=new double[m];
+			int m1=1;
 			for(int a=0; a<m;a++)
 			{
 				int i=(int) Math.floor(Math.random()*10+1);
@@ -84,8 +78,7 @@ public class exam1 {
 				ai[a]=i;
 				aj[a]=j;
 				ax[a]=x;
-				int m1=1;
-				ax[a]=x;
+				
 				if(x%2==1) {
 				System.out.println("(" + m1 + ") "+ i +" * " + j + "=");
 				}else {
@@ -105,20 +98,21 @@ public class exam1 {
 			}else {
 				if(ai[a1]%aj[a1]==0)
 				{
-					System.out.println("("+ m2 +") "+ ai[a1] +" / "+ aj[a1] +" = "+floor((ai[a1]/aj[a1])));
+					System.out.println("("+ m2 +") "+ ai[a1] +" / "+ aj[a1] +" = "+floor(ai[a1]/aj[a1]));
 				}else {
-			System.out.println("("+ m2 +") "+ ai[a1] +" / "+ aj[a1] +" = "+floor((ai[a1]/aj[a1]))+"..."+B[m-1]);
+					System.out.println("("+ m2 +") "+ ai[a1] +" / "+ aj[a1] +" = "+floor((ai[a1]/aj[a1]))+"..."+B[m-1]);
 			}
 				}
 			m2++;
 		}
 		
-			
+	
 	}
 
 		private static String floor(double d) {
 			// TODO Auto-generated method stub
 			return null;
 		}
+		
 	
 }
