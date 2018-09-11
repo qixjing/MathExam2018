4a22966eb8af9778b7d2ec3878bfd351334014a9
qiudongbao <392500514@qq.com>
2018-09-12 02:56:45 +0800

    任务一任务二完整代码

--- PSP6325/MathExam6325.java
+++ PSP6325/MathExam6325.java
@@ -11,15 +11,107 @@ import java.util.Scanner;
 public class MathExam6325 {
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
+		System.out.println("请输入需要生成的题目数量(范围：1-100)以及几年级的题目(1或2年级)：");
 		int n;
-		int grade = 1;
+		int grade;
+		int flag = 0;// 输入最后是否以空格结尾
 		Scanner sc = new Scanner(System.in);
 		String input = sc.nextLine();
-		n = Integer.valueOf(input);
-		if (n >= 1 && n <= 100) {
-			getFirstGradeMath(n, grade);
-		} else
-			System.out.println("警告！ 输入格式有误！");
+		String regex = "(([1-9]|[1-9][0-9]|100)( ?)(1?|2{1}))";// 符合输入格式的正则表达式
+		if (input.matches(regex)) {
+			for (int i = 0; i < input.length(); i++) { // 只输入了题目数量
+				if ((input.substring(i, i + 1)).equals(" ")) {
+					flag = 1;
+					break;
+				}
+			}
+			if (flag == 0) { // 不是以空格结尾
+				n = Integer.valueOf(input);
+				grade = 1; // 默認一年級
+				getFirstGradeMath(n, grade);
+				return;
+			}
+			if (input.endsWith(" ")) { // 只输入题目数 + 一个空格
+				n = Integer.valueOf(input.substring(0, input.length() - 1));
+				grade = 1;
+				getFirstGradeMath(n, grade);
+				return;
+			} else { // 输入格式正确 题目数 + 空格 + 几年级
+				String[] spiltArray = null;
+				spiltArray = input.split(" ");// 用空格分割字符串 分割成兩個數組
+				n = Integer.valueOf(spiltArray[0]);
+				grade = Integer.valueOf(spiltArray[1]);
+				if (grade == 1) {
+					getFirstGradeMath(n, grade);
+				} else {
+					getSecondGradeMath(n, grade);
+				}
+				return;
+			}
+		} else {
+			System.out.println(
+					"抱歉 輸入格式有誤" + "正確輸入格式:\n生成的题目数(範圍：1-100)" + " " + "生成几年级(1或2年级)" + "\n注意:若未指定年级，则默认生成一年级数学题");
+		}
+	}
+
+	private static void getSecondGradeMath(int n, int grade) {
+		// TODO 自动生成的方法存根
+		String[] Calculate = new String[n + 1];// 定义两个数组分别用來存放算式和计算结果
+		String[] Result = new String[n + 1];
+		Calculate[0] = "";
+		Result[0] = null;
+		for (int i = 1; i <= n; i++) {
+			int numberOne;
+			int numberTwo;
+			int number;
+			String randomSign = null;
+			Random randomNumber = new Random();
+			numberOne = randomNumber.nextInt(9) + 1;/* 取两个在1-9之间的随机整数作为算式的因子 */
+			numberTwo = randomNumber.nextInt(9) + 1;
+			number = randomNumber.nextInt(9);/* 第三個隨機數決定符號 大於4為‘*’ 反之為‘/’ */
+			if (number > 4) {
+				randomSign = "×";
+			} else {
+				randomSign = "÷";
+			}
+			Calculate[i] = "(" + i + ")" + " " + numberOne + ' ' + randomSign + ' ' + numberTwo + " = ";
+			// 定义商和余数
+			String quotient = null;
+			String remainder = null;
+			// 判断算式是乘法运算还是除法运算
+			if (randomSign.equals("÷")) {// 当算式为除法时，重新生成两个因数，使得两个因数小于81
+				numberOne = randomNumber.nextInt(80) + 1;
+				numberTwo = randomNumber.nextInt(80) + 1;
+				// 判断被除数是否大于除数
+				if (numberOne > numberTwo) {// 判断是否能够整除
+					if ((numberOne % numberTwo) == 0) {
+						quotient = numberOne / numberTwo + "";
+						Result[i] = quotient;
+					} else {
+						quotient = numberOne / numberTwo + "";
+						remainder = numberOne % numberTwo + "";
+						Result[i] = quotient + "..." + remainder;
+					}
+					// 修改因数后要相应的修改算式
+					Calculate[i] = "(" + i + ")" + " " + numberOne + ' ' + randomSign + ' ' + numberTwo + " = ";
+				} else {
+					// 当被除数小于除数时将被除数和除数互换
+					if ((numberTwo % numberOne) == 0) {
+						quotient = numberTwo / numberOne + "";
+						Result[i] = quotient;
+					} else {
+						quotient = numberTwo / numberOne + "";
+						remainder = numberTwo % numberOne + "";
+						Result[i] = quotient + "..." + remainder;
+					}
+					Calculate[i] = "(" + i + ")" + " " + numberTwo + randomSign + numberOne + " = ";
+				}
+			} else {
+				Result[i] = numberOne * numberTwo + "";
+			}
+		}
+		outputAnswer(Calculate, Result);
+		getAnswer();
 	}
 
 	private static void getFirstGradeMath(int n, int grade) {
