3f68281f4ac563cc76389a89276bb974cd4a1be4
tt9250872 <42865280+tt9250872@users.noreply.github.com>
2018-09-11 12:47:19 +0800

    第三次更新
    输出一次后可以选择继续出题或者退出。（继续后，上一次TXT文件数据会被覆盖）

--- PSP6343/MathExam6343.java
+++ PSP6343/MathExam6343.java
@@ -10,7 +10,6 @@ public class MathExam6343 {
 		File file = new File("out6343.txt");		//输出的文件
 		int []n =new int [args.length] ;	//创建传入参数数组
 		Scanner sc = new Scanner(System.in);			
-
 		boolean flag = true;
 		boolean flag1 = true;
 		
@@ -38,12 +37,12 @@ public class MathExam6343 {
 	    			break;
 	    	}
 	    }
+	    for(;;) {
 		if(!file.exists())						//判断TXT是否存在
 		{
 		try {
 			file.createNewFile();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		System.out.println("out6343.txt创建完成");
@@ -53,21 +52,16 @@ public class MathExam6343 {
 		try {
 			fw = new FileWriter(file);
 		} catch (IOException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		BufferedWriter bw = new BufferedWriter(fw);		
 
-
-		//int g=sc.nextInt();
 		int [] as = new int[n[0]+5];
 		int [] bs = new int[n[0]+5];
 		char [] cs = new char[n[0]+5];
 		int [] ds = new int[n[0]+5];
 		int[] es = new int [n[0]+5];
-		//int x = sc.nextInt();
-		
-	
+			
 		if(n[1]==1) {
 			for(int i =1;i<=n[0];i++) {			
 			int a=(int)(Math.random()*100+1);   
@@ -149,24 +143,18 @@ public class MathExam6343 {
 		}
 		}	
 		for(int i=1;i<=n[0];i++) {
-			//System.out.print("("+i+")");
 			try {
 				bw.write(String.valueOf("("+i+")"));
 				bw.write(String.valueOf(as[i]));	
-			//System.out.print(cs[i]);
-			bw.write(String.valueOf(cs[i]));	
-			//System.out.println(bs[i]);
-			bw.write(String.valueOf(bs[i]));
-			bw.newLine();
+				bw.write(String.valueOf(cs[i]));	
+				bw.write(String.valueOf(bs[i]));
+				bw.newLine();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			//System.out.print(as[i]);
 			
-		}
-		//System.out.println("-------------标准答案--------------");
-		
+		}		
 		try {
 			bw.write(String.valueOf("-------------标准答案--------------"));
 			bw.newLine();
@@ -179,21 +167,16 @@ public class MathExam6343 {
 			//System.out.print("("+i+")");
 			try {
 				bw.write(String.valueOf("("+i+")"));
-				//System.out.print(as[i]);
-			bw.write(String.valueOf(as[i]));
-			//System.out.print(cs[i]);
-			bw.write(String.valueOf(cs[i]));
-			//System.out.print(bs[i]);
-			bw.write(String.valueOf(bs[i]));
-			//System.out.print("="+ds[i]);
-			bw.write(String.valueOf("="+ds[i]));
+				bw.write(String.valueOf(as[i]));
+				bw.write(String.valueOf(cs[i]));
+				bw.write(String.valueOf(bs[i]));
+				bw.write(String.valueOf("="+ds[i]));
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 
 			if(es[i] != 0) {
-				//System.out.println("...."+es[i]);
 				try {
 					bw.write(String.valueOf("...."+es[i]));
 					bw.newLine();
@@ -222,4 +205,31 @@ public class MathExam6343 {
 			e.printStackTrace();
 		}
 	
-	}}
+		System.out.println("请选择退出(0)或者继续(1)！");
+		int g = sc.nextInt();
+		if(g==1) {
+			n[0]=sc.nextInt();
+			n[1]=sc.nextInt();
+				continue;
+			}
+		else if(g==0){
+				System.out.println("谢谢使用！");		
+				break;		
+			}
+		else {
+			System.out.print("重新输入");
+			while((boolean) flag) {
+				if(g==1 || g==0) {
+					flag= false;
+				}
+				else {
+					g=sc.nextInt();
+					if(g==1 || g==0)
+						break;
+				}
+			}
+		}
+
+		}
+		}
+	}
