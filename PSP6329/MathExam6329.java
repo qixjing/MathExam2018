22ce4648ac60f0131d50b35f44831f351f4431a6
wzx-Erica <42890929+wzx-Erica@users.noreply.github.com>
2018-09-11 17:53:33 +0800

    更改
    加了控制台界面输出，命令行输入异常处理，时间日期

--- PSP6329/MathExam6329.java
+++ PSP6329/MathExam6329.java
@@ -1,4 +1,4 @@
-import java.io.BufferedReader;				
+import java.io.BufferedReader;					
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
@@ -8,13 +8,17 @@ import java.util.Random;
 
 public class MathExam6329{
 public static void main(String[] args)throws IOException {    
-	String s;
-	args=new String[2];
-	int x=0,y=0;
+		int x=0,y=0;
+		String fh="";
 		x=Integer.parseInt(args[0]);
 		y=Integer.parseInt(args[1]);
 	//将字符串参数强制转换成int型
-
+		if(x<=0)System.out.println("题目数输入错误");
+		if(x>9999)System.out.println("题目数输入过大");
+		if(y!=1&&y!=2)System.out.println("年级输入错误");
+		if(x<=9999&&x>0&&(y==1||y==2))
+			//排错
+{	
 	File file = new File ("out.txt");
 	if(!file.exists())	
 	{
@@ -41,8 +45,8 @@ public static void main(String[] args)throws IOException {
 		for(;;){
 			number = (int)Math.floor((r.nextDouble()*100.0));a[m]=number;
 			number = (int)Math.floor((r.nextDouble()*100.0));c[m]=number;
-			if(b[m]==1)if(a[m]+c[m]<100)break;
-			if(b[m]==2)if(a[m]-c[m]>=0)break;
+			if(b[m]==1)if(a[m]+c[m]<100){fh="+";break;}
+			if(b[m]==2)if(a[m]-c[m]>=0){fh="-";break;}
 		}
 		}
 		//一年级
@@ -53,17 +57,18 @@ public static void main(String[] args)throws IOException {
 				for(;;){
 				number = (int)Math.floor((r.nextDouble()*100.0));a[m]=number;
 				number = (int)Math.floor((r.nextDouble()*100.0));c[m]=number;
-				if(b[m]==1)if(a[m]+c[m]<100)break;
-				if(b[m]==2)if(a[m]-c[m]>=0)break;
+				if(b[m]==1)if(a[m]+c[m]<100){fh="+";break;}
+				if(b[m]==2)if(a[m]-c[m]>=0){fh="-";break;}
 			}
 			else for(;;){
 				number = (int)Math.ceil((r.nextDouble()*99.0));a[m]=number;
 				number = (int)Math.ceil((r.nextDouble()*9.0));c[m]=number;
-				if(b[m]==3)if(a[m]*c[m]<100)break;
-				if(b[m]==4)if(a[m]%c[m]==0&&a[m]>=c[m])break;
+				if(b[m]==3)if(a[m]*c[m]<100){fh="*";break;}
+				if(b[m]==4)if(a[m]%c[m]==0&&a[m]>=c[m]){fh="/";break;}
 			}
 		}
-		}//二年级
+		}//二年级	 	
+		System.out.println("("+no+") "+a[m]+" "+fh+" "+c[m]+" = ");
 		bw.write(String.valueOf(a[m]));
 		bw.write(String.valueOf(' '));
 		if(b[m]==1)bw.write(String.valueOf('+'));
@@ -78,22 +83,24 @@ public static void main(String[] args)throws IOException {
 	 	no++;
 	 	m++;
 	 	bw.newLine();
+
 	}	bw.write(String.valueOf(" -----------------------标准答案 -----------------------"));
 	bw.newLine();
+	System.out.println(" -----------------------标准答案 -----------------------");
 	int i=0,z=0;
-	for(no=1;i<m;i++)
+	for(no=1;i<m;i++,no++)
 	{
-		{	
+		{			 	
 			bw.write(String.valueOf('('));
 			bw.write(String.valueOf(no));
 			bw.write(String.valueOf(')'));
 			bw.write(String.valueOf(' '));
 			bw.write(String.valueOf(a[i]));
 			bw.write(String.valueOf(' '));
-			if(b[i]==1){bw.write(String.valueOf('+'));z=a[i]+c[i];}
-			else if(b[i]==2){bw.write(String.valueOf('-'));z=a[i]-c[i];}
-			else if(b[i]==3){bw.write(String.valueOf('*'));z=a[i]*c[i];}
-			else if(b[i]==4){bw.write(String.valueOf('/'));z=a[i]/c[i];}
+			if(b[i]==1){bw.write(String.valueOf('+'));z=a[i]+c[i];fh="+";}
+			else if(b[i]==2){bw.write(String.valueOf('-'));z=a[i]-c[i];fh="-";}
+			else if(b[i]==3){bw.write(String.valueOf('*'));z=a[i]*c[i];fh="*";}
+			else if(b[i]==4){bw.write(String.valueOf('/'));z=a[i]/c[i];fh="/";}
 			bw.write(String.valueOf(' '));
 		 	bw.write(String.valueOf(c[i]));
 		 	bw.write(String.valueOf(' '));
@@ -101,8 +108,10 @@ public static void main(String[] args)throws IOException {
 		 	bw.write(String.valueOf(' '));
 		 	bw.write(String.valueOf(z));
 		 	bw.newLine();
+		 	System.out.println("("+no+") "+a[i]+" "+fh+" "+c[i]+" = "+z);
 		}
-//		时间
 	}bw.close();
+	System.out.println("      211606329  王浩  2018年9月11日 17:42:28");
+	}
 }
 }
