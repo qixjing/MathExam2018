250fa79f14695de06c150de9767059bd323a1a34
1429189566 <1429189566@qq.com>
2018-09-11 19:51:39 +0800

    new !
    修改了输入年级的BUG

--- PSP李震211606316/exam1.java
+++ PSP李震211606316/exam1.java
@@ -14,12 +14,13 @@ public class exam1 {
 		Scanner s=new Scanner(System.in);
 		System.out.println("请输入年级：");
 		int grade = s.nextInt();
-		if(grade == 1) {
-			grade_one();
-		}else if(grade ==2) {
+		if(grade == 2) { //判断年级，默认为1年级
 			grade_two();
+		}else { 
+			grade_one();
 		}
 		}
+		
 
 	
 		public static void grade_one() {	
@@ -32,20 +33,18 @@ public class exam1 {
 			int m1=1;
 			for(int a=0; a<n;a++)
 			{
-				int i=(int) Math.floor(Math.random()*20+1);
-				int j=(int) Math.floor(Math.random()*20+1);
-				int x=(int) Math.floor(Math.random()*2+1);
-				ai[a]=i;
-				aj[a]=j;
-				ax[a]=x;
-//				int m1=1;
-				ax[a]=x;
-				if(x%2==1) {
+				int i=(int) Math.floor(Math.random()*20+1); //生成随机数，规定范围，并让他不为0
+				int j=(int) Math.floor(Math.random()*20+1); //生成随机数，规定范围，并让他不为0
+				int x=(int) Math.floor(Math.random()*2+1); //生成随机数，规定范围，并让他不为0
+				ai[a]=i; //将随机数存入数组
+				aj[a]=j;//将随机数存入数组
+				ax[a]=x;//将随机数存入数组
+				if(x%2==1) {		//判断加减法
 				System.out.println("(" + m1 + ") "+ i +" + " + j + "=");
 				}else {
 				System.out.println("(" + m1 + ") "+ i + " - "+j+"= ");
 				}
-				m1++;
+				m1++;  //序号持续增加
 				}
 				
 			System.out.println("                  ");		
@@ -66,19 +65,18 @@ public class exam1 {
 			System.out.println("请输入需要出题的数目：");
 			Scanner s=new Scanner(System.in);
 			int m = s.nextInt();
-			double ai[]=new double[m];
+			double ai[]=new double[m]; 
 			double aj[]=new double[m];
 			double ax[]=new double[m];
 			int m1=1;
 			for(int a=0; a<m;a++)
 			{
-				int i=(int) Math.floor(Math.random()*10+1);
-				int j=(int) Math.floor(Math.random()*10+1);
-				int x=(int) Math.floor(Math.random()*2+1);
-				ai[a]=i;
-				aj[a]=j;
-				ax[a]=x;
-				
+				int i=(int) Math.floor(Math.random()*10+1);//生成随机数，规定范围，并让他不为0
+				int j=(int) Math.floor(Math.random()*10+1);//生成随机数，规定范围，并让他不为0
+				int x=(int) Math.floor(Math.random()*2+1);//生成随机数，规定范围，并让他不为0
+				ai[a]=i;//将随机数存入数组
+				aj[a]=j;//将随机数存入数组
+				ax[a]=x;//将随机数存入数组
 				if(x%2==1) {
 				System.out.println("(" + m1 + ") "+ i +" * " + j + "=");
 				}else {
@@ -90,8 +88,8 @@ public class exam1 {
 			int m2=1;
 			for(int a1=0;a1<m;a1++)
 			{
-				int B[]=new int[m];
-			    B[m-1]=(int) (ai[a1]%aj[a1]);
+				int B[]=new int[m];  
+			    B[m-1]=(int) (ai[a1]%aj[a1]); //将余数存入数组
 			if(ax[a1]%2==1) 
 			{
 			System.out.println("("+ m2 +") "+ ai[a1] +" * "+ aj[a1] +" = "+(ai[a1]*aj[a1]));
