6ba9e7550d0b02213dceb4dfddf8c26825e1c0e2
pikapika1210 <40065317+pikapika1210@users.noreply.github.com>
2018-09-11 19:13:42 +0800

    二年级计算题自动生成1.0
    能够选择一二年级并且能够自动生成四种运算题目，但除法方法的代码还存在缺陷

--- PSP5240/MathExam5240.java
+++ PSP5240/MathExam5240.java
@@ -8,7 +8,20 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class MathExam5240 {
-	
+	public static int multiplication(int i,OutputStream out) {
+		int a =(int) (Math.random()*10);
+		int b =(int) (Math.random()*10);
+		String word=("("+(i+1)+") "+a +" * "+ b +" = ")+"\n";
+		try {
+			out.write(word.getBytes());
+		} catch (IOException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		}
+		return a*b;
+		
+	}
+
 	public static int addition(int i,OutputStream out) {
 		int a =(int) (Math.random()*100);
 		int b =(int) (Math.random()*(100-a+1));
@@ -33,20 +46,44 @@ public class MathExam5240 {
 		}
 		return a-b;
 	}
-	public static void math(int n){
+	public static void math(int n,int grade){
+		int j;
+		if(grade==1) {
+			j=2;
+		}else {
+			j=4;
+		}
 		int intArray[] = new int[n];
+		int intArray1[] = new int[n];
 		String filename="out";
 		  File file = new File(filename+".txt"); 
 		  OutputStream out;
 		try {
 			out = new FileOutputStream(file);
+			
 			for(int i=0;i<n;i++)
-		{
-			if ((int) (Math.random()*2)==0) {//进行加法计算
+		{	int k=(int) (Math.random()*j);
+			if (k==0) {//进行加法计算
 				intArray[i]=addition(i,out);
-		}else {//进行减法计算
+				intArray1[i]=0;
+		}else if(k==1){//进行减法计算
 			intArray[i]=subtraction(i,out);
-
+			intArray1[i]=0;
+		}else if(k==2) {
+			intArray[i]=multiplication(i,out);
+			intArray1[i]=0;
+		}else{
+			int a =(int) (Math.random()*100);
+			int b =(int) (1+Math.random()*9);
+			String word=("("+(i+1)+") "+a +" / "+ b +" = ")+"\n";
+			try {
+				out.write(word.getBytes());
+			} catch (IOException e) {
+				// TODO 自动生成的 catch 块
+				e.printStackTrace();
+			}
+			intArray[i]=a/b;
+			intArray1[i]=a%b;
 		}
 			
 		}
@@ -57,7 +94,12 @@ public class MathExam5240 {
 			  Scanner scanner0=new Scanner(file);
 			  int i=0;
 			  while(scanner0.hasNextLine()&&(i!=n)) {
+				  if(intArray1[i]==0) {
 				  word =(scanner0.nextLine())+intArray[i]+"\n";
+				  }
+				  else {
+				  word =(scanner0.nextLine())+intArray[i]+"..."+intArray1[i]+"\n"; 
+				  }
 				  out.write(word.getBytes());
 				  i++;
 			  }
@@ -73,53 +115,46 @@ public class MathExam5240 {
 		}
 
 	}
-	public static int Input(String[] args) {
+	public static boolean Input(String[] args) {
 		String regex="\\d*";
-		if(args.length!=1) {//解决输入参数为多个
-			System.out.println("请输入单个参数。");
-			Scanner input=new Scanner(System.in);
-			args[0]=input.nextLine();
+		if(args.length!=2) {//解决输入参数为多个
+			System.out.println("输入的参数应为两个参数");
 		}
-		
-		int n=nonnumeric(regex,args);
-		while(n>30 || n<0) {
-			System.out.println("请输入一个不大于30不小于0的数字");
-			Scanner input=new Scanner(System.in);
-			args[0]=input.nextLine();
-			n=nonnumeric(regex,args);
-		}
-		return n;
-		
-	}
-	public static int nonnumeric(String regex,String[] args) {
-		
-		while(!args[0].matches(regex)) {//解决如果输入不为数字
-			System.out.println("请输入正确的数字。");
-			Scanner input=new Scanner(System.in);
-			args[0]=input.nextLine();
-		
+		else if((!args[0].matches(regex))||(!args[1].matches(regex))) {//解决如果输入不为数字
+			System.out.println("输入的参数应为数字且不为-1");
 			}
-			String s = String.valueOf(args[0]);
-			int n = Integer.parseInt(s);
-			return n;
-			
+		else{String s = String.valueOf(args[0]);
+		String s1=String.valueOf(args[1]);
+		int n = Integer.parseInt(s);
+		int grade =Integer.parseInt(s1);
+		if(n<30 && (grade==1||grade==2)) {
+			return true;
+		}else {
+			System.out.println("输入的题数应小于30题且只能选择一年级或者二年级");
+		}
+		}
+		return false;
 	}
 	public static void main(String[] args){
 		// TODO 自动生成的方法存根
 		System.out.println("---------------欢迎使用MathExam5240程序---------------");
-		System.out.println("1.请按照格式：输入“题目数量（数字）”。 例：8，即程序自动生成八道算数题。");
+		System.out.println("1.请按照格式：输入“题目数量（数字）+空格+年级（数字）”。 例：8 2，即程序自动生成二年级八道算数题。");
 		System.out.println("2.程序最多一次可生成30道算术题。学习是一个循序渐进的过程，不宜操之过急。");
 		System.out.println("3.使用过程中若遇到无法解决的bug，可联系博客：乌鸦君一米八进行代码改进。");
 		System.out.println("-----------------------------------------------------");
-		int n=Input(args);
+		if(Input(args)) {
+			String s = String.valueOf(args[0]);
+			String s1=String.valueOf(args[1]);
+			int n = Integer.parseInt(s);
+			int grade =Integer.parseInt(s1);
 		try {
-			math(n);
+			math(n,grade);
 		} catch (Exception e) {
 			// TODO 自动生成的 catch 块
 			e.printStackTrace();
 		}
 		
-
+		}
 	}
 
 }
