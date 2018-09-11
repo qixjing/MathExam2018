e3eb44871d5fea808757f670a4cb526d520bf062
Dollarss <43141953+Dollarss@users.noreply.github.com>
2018-09-12 00:35:39 +0800

    小学二年级乘法除法也有一年级的

--- PSP6306/MathExam6306.java
+++ PSP6306/MathExam6306.java
@@ -10,16 +10,23 @@ public class MathExam6306 {
 
 	public static void main(String[] args) throws FileNotFoundException {		
 		// TODO 自动生成的方法存根
-		int n;
+		int n,g;
 		Scanner scanner = new Scanner(System.in);
 		// 从键盘接收数据
 		n = scanner.nextInt();
 		 // 接收的数据赋值给n
+		g = scanner.nextInt();
 		String[] str = new String[n];
 		File file = new File("out6306.txt");
 		PrintStream ps = new PrintStream(file);
 		System.setOut(ps);
-		zzz(n,str);
+		if (g == 1) {
+			zzz(n, str);
+		} else if (g==2) {
+			xxx(n, str);
+		} else {
+			System.out.println("不会做");
+		}
 	}
 	public static void zzz(int n,String[] str) {
 		int x;
@@ -52,6 +59,37 @@ public class MathExam6306 {
 
 		
 	}
+	public static void xxx(int n,String[] str) {
+		int x;
+		int y;
+		for (int i = 0; i < n; i++) {
+			int z = (int)(Math.random()*(2));
+			x = (int)(Math.random()*9)+1;
+			y = (int)(Math.random()*9)+1;
+			
+		if(z==0) {
+			System.out.println("("+(i+1)+")" + "" + x + "*" + y + "" + "=" + " " );
+			str[i] = "(" + (i+1) + ")" +" " + x + "*" + y + " " + "=" + " " + (x*y);}
+		else{
+			if(x-y<0) {
+				System.out.println("("+(i+1)+")" + "" + y + "/" + x + "" + "=" + " " );
+				str[i] = "(" + (i+1) + ")" +" " + y + "/" + x + " " + "=" + " " + (y/x);
+			}
+			else {
+				System.out.println("("+(i+1)+")" + "" + x + "/" + y + "" + "=" + " " );
+				str[i] = "(" + (i+1) + ")" +" " + x + "/" + y + " " + "=" + " " + (x/y);
+		}
+			}
+		}
+		System.out.println("标准答案");
+		for(String s:str)
+			System.out.println(s);
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日 HH:mm");
+		System.out.println("              211606306 姓名：黄柏涛  " +sdf.format(new Date()));
+
 		
+		
+		
+	}
 		
 }
