c7219358f7394534a0971d179d60854ad0a9fe27
tt9250872 <42865280+tt9250872@users.noreply.github.com>
2018-09-11 14:03:32 +0800

    第四次更新
    修改了继续输出后不能修改年级和题目数据，只能继续输出命令段输入的数据。
    更新了输出结束后把学号姓名以及当前时间写入TXT中。
    修改了for循环的位置。

--- PSP6343/MathExam6343.java
+++ PSP6343/MathExam6343.java
@@ -3,17 +3,24 @@ import java.io.File;
 //import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 //import java.io.OutputStream;
 public class MathExam6343 {
 	public static void main(String[] args) {
+		//for(;;) {
 		File file = new File("out6343.txt");		//输出的文件
 		int []n =new int [args.length] ;	//创建传入参数数组
 		Scanner sc = new Scanner(System.in);			
+
 		boolean flag = true;
 		boolean flag1 = true;
-		
-		n[0] = Integer.parseInt(args[0]);	//对第一个传入参数限制
+		boolean flag2 = true;
+		n[0] = Integer.parseInt(args[0]); 
+	    n[1] = Integer.parseInt(args[1]);		
+		for(;;) {
+	//对第一个传入参数限制
 		while (flag) {
 			if(n[0]>0 && n[0]<9999)
 				flag = false;
@@ -26,7 +33,7 @@ public class MathExam6343 {
 			}
 		}
 		
-	    n[1] = Integer.parseInt(args[1]);	//对第二个传入参数限制
+	//对第二个传入参数限制
 	    while(flag1) {
 	    	if(n[1]==1 || n[1]==2)
 	    		flag1 = false;
@@ -37,12 +44,13 @@ public class MathExam6343 {
 	    			break;
 	    	}
 	    }
-	    for(;;) {
+	   
 		if(!file.exists())						//判断TXT是否存在
 		{
 		try {
 			file.createNewFile();
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		System.out.println("out6343.txt创建完成");
@@ -52,16 +60,21 @@ public class MathExam6343 {
 		try {
 			fw = new FileWriter(file);
 		} catch (IOException e1) {
+			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		BufferedWriter bw = new BufferedWriter(fw);		
 
+
+		//int g=sc.nextInt();
 		int [] as = new int[n[0]+5];
 		int [] bs = new int[n[0]+5];
 		char [] cs = new char[n[0]+5];
 		int [] ds = new int[n[0]+5];
 		int[] es = new int [n[0]+5];
-			
+		//int x = sc.nextInt();
+		
+	
 		if(n[1]==1) {
 			for(int i =1;i<=n[0];i++) {			
 			int a=(int)(Math.random()*100+1);   
@@ -143,18 +156,24 @@ public class MathExam6343 {
 		}
 		}	
 		for(int i=1;i<=n[0];i++) {
+			//System.out.print("("+i+")");
 			try {
 				bw.write(String.valueOf("("+i+")"));
 				bw.write(String.valueOf(as[i]));	
-				bw.write(String.valueOf(cs[i]));	
-				bw.write(String.valueOf(bs[i]));
-				bw.newLine();
+			//System.out.print(cs[i]);
+			bw.write(String.valueOf(cs[i]));	
+			//System.out.println(bs[i]);
+			bw.write(String.valueOf(bs[i]));
+			bw.newLine();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			//System.out.print(as[i]);
 			
-		}		
+		}
+		//System.out.println("-------------标准答案--------------");
+		
 		try {
 			bw.write(String.valueOf("-------------标准答案--------------"));
 			bw.newLine();
@@ -167,16 +186,21 @@ public class MathExam6343 {
 			//System.out.print("("+i+")");
 			try {
 				bw.write(String.valueOf("("+i+")"));
-				bw.write(String.valueOf(as[i]));
-				bw.write(String.valueOf(cs[i]));
-				bw.write(String.valueOf(bs[i]));
-				bw.write(String.valueOf("="+ds[i]));
+				//System.out.print(as[i]);
+			bw.write(String.valueOf(as[i]));
+			//System.out.print(cs[i]);
+			bw.write(String.valueOf(cs[i]));
+			//System.out.print(bs[i]);
+			bw.write(String.valueOf(bs[i]));
+			//System.out.print("="+ds[i]);
+			bw.write(String.valueOf("="+ds[i]));
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 
 			if(es[i] != 0) {
+				//System.out.println("...."+es[i]);
 				try {
 					bw.write(String.valueOf("...."+es[i]));
 					bw.newLine();
@@ -208,6 +232,7 @@ public class MathExam6343 {
 		System.out.println("请选择退出(0)或者继续(1)！");
 		int g = sc.nextInt();
 		if(g==1) {
+			System.out.println("输入题目数和年级数");
 			n[0]=sc.nextInt();
 			n[1]=sc.nextInt();
 				continue;
@@ -217,19 +242,27 @@ public class MathExam6343 {
 				break;		
 			}
 		else {
-			System.out.print("重新输入");
-			while((boolean) flag) {
+			System.out.println("重新输入0或1");
+			g=sc.nextInt();
+			while(flag2) {
 				if(g==1 || g==0) {
-					flag= false;
+					flag2= false;
 				}
 				else {
+					System.out.println("重新输入0或1");
 					g=sc.nextInt();
 					if(g==1 || g==0)
 						break;
 				}
 			}
+			System.out.println("输入题目数和年级数");
+			n[0] = sc.nextInt();
+			n[1] = sc.nextInt();
 		}
 
 		}
+	    SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日 HH:mm");
+	    String time = sdf.format(new Date());
+	    System.out.println("211606343 杨宇潇" + time);
 		}
 	}
