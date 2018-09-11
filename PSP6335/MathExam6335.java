fda2c911f2b113d4c569e4536cbd82b35aa6f4b6
Wuyiz <42826957+Wuyiz@users.noreply.github.com>
2018-09-11 19:38:40 +0800

    MathExam_V1.0.8
    /*
     * 完成一个命令行工具MathExam，自动生成一份小学一年级的数学四则运算题。
     *     1.增加了对年级参数的识别。；
     *     2.确保命令行接受的参数是空、一个参数、两个参数和多个参数都能运行。
     *     【默认值——空参数：题数为”10“，年级为”2“；    一个参数：年级默认值为”2“； 
     *     						两个参数无需使用默认值；       多个参数只接受前两个参数值。】
     *     【ps：还存在输入“00000000001”的识别错误bug，待修正】
     * 		coding ： GBK
     * 		MathExam_V1.0.8
     */

--- PSP6335/MathExam6335.java
+++ PSP6335/MathExam6335.java
@@ -3,6 +3,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.regex.Matcher;
@@ -10,13 +12,15 @@ import java.util.regex.Pattern;
 
 public class MathExam6335 {
 	/*
-	 * 完成一个命令行工具MathExam，自动生成一份小学一年级的数学四则运算题。
-	 *     1.为除法运算方法添加了余数计算结果；
-	 *     2.取消了输入完参数后在控制台显示题目的功能。
-	 *     【ps：还存在输入“00000000001”的识别错误bug，待修正】
-	 * 		coding ： GBK
-	 * 		MathExam_V1.0.7
-	 */	
+ * 完成一个命令行工具MathExam，自动生成一份小学一年级的数学四则运算题。
+ *     1.增加了对年级参数的识别。；
+ *     2.确保命令行接受的参数是空、一个参数、两个参数和多个参数都能运行。
+ *     【默认值——空参数：题数为”10“，年级为”2“；    一个参数：年级默认值为”2“； 
+ *     						两个参数无需使用默认值；       多个参数只接受前两个参数值。】
+ *     【ps：还存在输入“00000000001”的识别错误bug，待修正】
+ * 		coding ： GBK
+ * 		MathExam_V1.0.8
+ */	
 	int firstNumber, secondNumber;		
 	int symbol;	
 	static int grade;
@@ -27,14 +31,31 @@ public class MathExam6335 {
 	String[] str_ArithmeticProblem = new String[100+1];	//存放算术题
 	String[] str_MathAnswer = new String[100+1];	//存放算术题及答案
 	
-	public MathExam6335() {
-		// TODO Auto-generated constructor stub
+	//输入的参数为空值
+	public MathExam6335(){
+		String count_Default = "10";	//定义参数默认值
+		String grade_Default = "2";
+		inPut(count_Default, grade_Default);
 		mathProblem(count);
-		
 		outPut();
 	}
 	
-	private static void inPut(String args0,String args1) {
+	//输入的参数只有一个
+	public MathExam6335(String args0){
+		String grade_Default = "2";
+		inPut(args0, grade_Default);
+		mathProblem(count);
+		outPut();
+	}
+	
+	//输入的参数个数正常
+	public MathExam6335(String args0,String args1){
+		inPut(args0, args1);
+		mathProblem(count);
+		outPut();
+	}
+
+	public void inPut(String str0,String str1) {
 		// TODO Auto-generated method stub
 		boolean flag1 = true;
 		boolean flag2 = true;
@@ -48,8 +69,8 @@ public class MathExam6335 {
 		
 		while (true) {
 			
-			matcher1 = pattern1.matcher(args0);
-			matcher2 = pattern2.matcher(args1);
+			matcher1 = pattern1.matcher(str0);
+			matcher2 = pattern2.matcher(str1);
 			
 			try {
 				flag1 = matcher1.matches();
@@ -58,18 +79,18 @@ public class MathExam6335 {
 				if(!flag1 || !flag2){	
 						throw new NumberFormatException();		
 				} else {	
-					count = Integer.valueOf(args0);
-					grade = Integer.valueOf(args1);
+					count = Integer.valueOf(str0);
+					grade = Integer.valueOf(str1);
 				}
 			} catch (NumberFormatException e) {
 				// TODO: handle exception
 				if(!flag1){
-					System.out.println("--------   只能输入99以内的题目数量，请按规定重新输入题目数量 :    --------");
-					args0 = in.next();
+					System.out.println("输入错误，只能输入99以内的题目数量，请按规定重新输入题目数量 : ");
+					str0 = in.next();
 				} 
 				if(!flag2){
-					System.out.println("--------   年级为\"1\"或\"2\" ，请按规定重新输入年级 :   --------");
-					args1 = in.next();
+					System.out.println("输入错误，年级为\"1\"或\"2\" ，请按规定重新输入年级 : ");
+					str1 = in.next();
 				}
 				continue;
 			}			
@@ -80,7 +101,11 @@ public class MathExam6335 {
 	private void outPut() {
 		// TODO Auto-generated method stub
 		File file = new File("out.txt");
-		
+		Date date = new Date();//创建一个时间对象，获取到当前的时间
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");//设置时间显示格式
+		String currentTime = "\r\n         211606335   吴沂章   " + sdf.format(date);//将当前时间格式化为需要的类型
+		byte[] bytesdate = currentTime.getBytes();
+
 		if(!file.exists()){	//判断文件是否存在
 			try {
 				file.createNewFile();
@@ -109,8 +134,11 @@ public class MathExam6335 {
 				fos.write(b_MathAnswer);
 				fos.write(bytes);
 			}
+			fos.write(bytesdate);
 			fos.flush();
 			fos.close();	//关闭文件输出流
+			
+			System.out.print("-------  本次共生成" + count + "道小学"+ grade + "年级算数题，请打开out.txt文件查看详情    -------");
 			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -122,7 +150,7 @@ public class MathExam6335 {
 		
 	}
 	
-	/*
+/*
 	 * 加法：
 	 *  1.一二年级的加法的两个加数在20以内。
 	 * 
@@ -176,8 +204,7 @@ public class MathExam6335 {
 	 * 除法：
 	 * 
 	 * 1.一二年级的除法运算应该在”九九乘法表“范围以内；
-	 * 2.分母不能为”0“;
-	 * 3.二年级有余数计算能力。
+	 * 2.分母不能为”0“。
 	 * 
 */
 	private void div(int i) {
@@ -247,8 +274,17 @@ public class MathExam6335 {
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		MathExam6335.inPut(args[0], args[1]);
-		new MathExam6335();
+		if(args.length == 0){
+			new MathExam6335();
+		} else if(args.length == 1){
+			new MathExam6335(args[0]);
+		}
+		else if(args.length == 2){
+			new MathExam6335(args[0], args[1]);
+		}else {
+			new MathExam6335(args[0], args[1]);
+		}
+		
 
 	}
 }
