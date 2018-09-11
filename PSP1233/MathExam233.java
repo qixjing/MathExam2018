8a385cd7837ebb59a1df6964983e3bbe9df6d435
zklowens <42697400+zklowens@users.noreply.github.com>
2018-09-11 12:43:11 +0800

    修改BUG
    缩小输入的题目数范围至1000以内

--- PSP1233/MathExam233.java
+++ PSP1233/MathExam233.java
@@ -53,6 +53,8 @@ public class MathExam233 {
 		for (int i = 0; i < n.length; i++) {
 			s1 = s1 + "\n" + n[i];
 		}
+		SimpleDateFormat df = new SimpleDateFormat("yyyy年MM月dd日   HH:mm:ss");//设置日期格式
+		System.out.println("         "+"211601233"+"张康凌"+df.format(new Date()));// new Date()为获取当前系统时间
 	}
 
 	private static void createtxt() {
@@ -108,15 +110,16 @@ public class MathExam233 {
 
 	public static void main(String[] args) {
 		boolean isNum = args[0].matches("[^0]\\d*");
-		if ((isNum == true && args.length == 1 && args[0].matches("^[0-9]*[1-9][0-9]*$")) || (isNum == true && args.length == 2 && args[1].matches("[1]")&& args[0].matches("^[0-9]*[1-9][0-9]*$"))) {
+		if ((isNum == true && args.length == 1 && args[0].matches("^[0-9]*[1-9][0-9]*$") && args[0].length()<4) || (isNum == true && args.length == 2 && args[1].matches("[1]")&& args[0].matches("^[0-9]*[1-9][0-9]*$")&& args[0].length()<4)) {
 			int num = Integer.valueOf(args[0]);
 			Math(num);
 			createtxt();
-		} else if (isNum == true && args.length == 2 && args[1].matches("[2]")&& args[0].matches("^[0-9]*[1-9][0-9]*$")) {
+		} else if (isNum == true && args.length == 2 && args[1].matches("[2]")&& args[0].matches("^[0-9]*[1-9][0-9]*$")&& args[0].length()<4) {
 			int num = Integer.valueOf(args[0]);
 			Math2(num);
 			createtxt();
-		} else {
+		}
+		else {
 			System.out.println("输入错误，请重新输入");
 		}
 	}
