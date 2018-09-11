4bdcdc3f74a2990fa169b12d240f840b41a2c2d0
xtlhq <41507303+xtlhq@users.noreply.github.com>
2018-09-11 18:03:24 +0800

    第二次提交
    把System输入改为参数输入

--- PSP6370/MathExam1.java
+++ PSP6370/MathExam1.java
@@ -4,15 +4,13 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 import java.util.Random;
-import java.util.Scanner;
 
 public class MathExam1{
 
 	public static void main(String[] args) {
 		Random ran = new Random();
 		int a;
-		Scanner reader = new Scanner(System.in);
-		int n=reader.nextInt();
+		int n=Integer.parseInt(args[0]);
 		String[] question = new String[n+1];
 		String[] answer = new String[n+1];
 		File file = new File ("out.txt");
@@ -21,9 +19,9 @@ public class MathExam1{
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		a=ran.nextInt(2);
 		for(int k=1;k<=n;k++) {
 			int i, j;
+			a=ran.nextInt(2);
 			i=ran.nextInt(100);
 			j=ran.nextInt(100);	
 			if(a==0) {
@@ -35,9 +33,12 @@ public class MathExam1{
 			if(a==1) {
 				int minus;
 				minus=i-j;
+				if(minus<0) {
+					k--;
+					continue;
+				}
 				question[k]="("+k+") "+i+" - "+j;
 				answer[k]="("+k+") "+i+" - "+j +" = "+ minus;
-				
 			}
 		}
 		for(int k=1;k<=n;k++) {
